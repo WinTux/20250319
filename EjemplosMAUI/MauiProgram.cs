46cf51e68bd59007ac99ef292a9856d325a3b462
WinTux <rusok.software@gmail.com>
2025-03-21 20:47:01 -0400

    Lector QR finalizado

--- EjemplosMAUI/MauiProgram.cs
+++ EjemplosMAUI/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using EjemplosMAUI.Paginas;
 using Microsoft.Extensions.Logging;
 using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
 
 namespace EjemplosMAUI
 {
@@ -16,7 +17,12 @@ namespace EjemplosMAUI
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-            builder.UseBarcodeReader();
+            builder.UseBarcodeReader()
+                .ConfigureMauiHandlers(h => {
+                    h.AddHandler(typeof(CameraBarcodeReaderView), typeof(CameraBarcodeReaderViewHandler));
+                    h.AddHandler(typeof(CameraView), typeof(CameraViewHandler));
+                    h.AddHandler(typeof(ZXing.Net.Maui.Controls.BarcodeGeneratorView), typeof(BarcodeGeneratorViewHandler));
+                });
             builder.Services.AddTransient<ReolojPage>();
             builder.Services.AddTransient<ScannerQRPage>();
 
--- EjemplosMAUI/Paginas/ScannerQRPage.xaml.cs
+++ EjemplosMAUI/Paginas/ScannerQRPage.xaml.cs
@@ -1,3 +1,5 @@
+using ZXing.Net.Maui;
+
 namespace EjemplosMAUI.Paginas;
 
 public partial class ScannerQRPage : ContentPage
@@ -5,5 +7,20 @@ public partial class ScannerQRPage : ContentPage
 	public ScannerQRPage()
 	{
 		InitializeComponent();
-	}
+        lectorCodigo.Options = new BarcodeReaderOptions()
+        {
+            AutoRotate = true,
+            Formats = BarcodeFormats.All,//BarcodeFormat.QrCode,
+            TryHarder = true,
+            Multiple = false
+        };
+
+    }
+	async void OnCodigoBarrasDetectado(object sender, BarcodeDetectionEventArgs e)
+    {
+		Dispatcher.Dispatch(() => {
+            //await DisplayAlert("Código de barras detectado", e.Result.Text, "Aceptar");
+            resultadoCodigo.Text = $"Texto: {e.Results[0].Value}; Formato: {e.Results[0].Format}";
+        });
+    }
 }
